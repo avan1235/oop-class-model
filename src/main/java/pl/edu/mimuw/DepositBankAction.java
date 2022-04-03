25d5c51f977d5900b2e855efeb7186edae77809d
Marcin <92443163+kapustaikwas27@users.noreply.github.com>
2022-04-03 23:03:11 +0200

    deposit

--- src/main/java/pl/edu/mimuw/DepositBankAction.java
+++ src/main/java/pl/edu/mimuw/DepositBankAction.java
@@ -1,9 +1,24 @@
 package pl.edu.mimuw;
 
-public abstract class DepositBankAction extends BankAction {
+import java.sql.Timestamp;
 
-  // TODO:
-  // - remove abstract modifier from this class
-  // - this class has to contain extra fields other than LoanBankAction
-  // - you have to design it's constructors/accessors/setters and implement some from BankAction
+public class DepositBankAction extends BankAction {
+
+  private final double interestRate;
+  private final double bonus;
+
+  public DepositBankAction(double amount, Timestamp begin, Timestamp end, double interestRate, double bonus) {
+    super(amount, begin, end);
+    this.interestRate = interestRate; 
+    this.bonus = bonus;
+  }
+  
+  public double getCollectedAmount(Timestamp moment) {
+    long miliseconds = moment.getTime() - begin.getTime();
+    return this.amount * (1.0 + interestRate) * (miliseconds / (1000 * 60 * 60 * 24)) / 365.0 + bonus;
+  }
+
+  public String toString() {
+    return String.valueOf("AMOUNT: " + this.amount + " BEGIN: " + this.begin + " END: " + this.end + " INTEREST RATE: " + this.interestRate + " BONUS: " + this.bonus);  
+  }
 }
