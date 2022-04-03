6323c1cb1caccfb9567aab5ebed6505c7adb716a
Marcin <92443163+kapustaikwas27@users.noreply.github.com>
2022-04-03 23:01:39 +0200

    bank

--- src/main/java/pl/edu/mimuw/Bank.java
+++ src/main/java/pl/edu/mimuw/Bank.java
@@ -1,24 +1,41 @@
 package pl.edu.mimuw;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
 
+  private final String name;
+  private final List<BankClient> allClients;
+
+  public Bank(String name) {
+    this.name = name;
+    allClients = new ArrayList<>();
+  }
+  
   @Override
   public String toString() {
-    throw new IllegalStateException("TODO: get information about bank and all its clients (use StringBuilder)");
+    StringBuilder s = new StringBuilder();
+    
+    for(BankClient client : allClients) {
+        s.append(client.getAccountNumber());
+        s.append(", ");
+    }
+        
+    String info = s.toString();
+    return "BANK: " + name + " CLIENTS: " + info;
   }
 
   public List<BankClient> getAllClients() {
-    throw new IllegalStateException("TODO: remember not to return internal state list as it can be modified");
+    return new ArrayList<>(allClients);
   }
 
   public void addClient(BankClient client) {
-    throw new IllegalStateException("TODO");
+    allClients.add(client);
   }
 
   public BankClient getClient(int clientIndex) {
-    final var clients = getAllClients();
+    final List<BankClient> clients = getAllClients();
     if (clientIndex >= clients.size()) throw new IllegalArgumentException("Invalid client index");
     return clients.get(clientIndex);
   }
