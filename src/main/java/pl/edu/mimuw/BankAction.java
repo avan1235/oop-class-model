93b414570cf30880f58927fff29f8e0831233d13
Artur Kamieniecki <kamart02@wp.pl>
2022-04-03 18:58:56 +0200

    Finished task

--- src/main/java/pl/edu/mimuw/BankAction.java
+++ src/main/java/pl/edu/mimuw/BankAction.java
@@ -4,16 +4,40 @@ import java.sql.Timestamp;
 
 public abstract class BankAction {
 
-  public double totalAmount() {
-    throw new IllegalStateException("TODO");
+  protected final double amount;
+  protected final double interestRate;
+  protected final Timestamp start;
+  protected final Timestamp end;
+  protected final double finalAmount;
+  protected boolean finished;
+
+  protected BankAction(double amount, double interestRate, Timestamp end) {
+    this.amount = amount;
+    this.interestRate = interestRate;
+    this.start = new Timestamp(System.currentTimeMillis());
+    this.end = end;
+    this.finalAmount = this.amount + this.amount * this.interestRate;
+    this.finished = false;
+  }
+  public BankAction(BankAction action) {
+    this.amount = action.amount;
+    this.interestRate = action.interestRate;
+    this.start = action.start;
+    this.end = action.end;
+    this.finalAmount = action.finalAmount;
+    this.finished = action.finished;
   }
 
+  public abstract double totalAmount();
+
+  public abstract BankAction copy();
+
   public Timestamp getStartTimestamp() {
-    throw new IllegalStateException("TODO");
+    return this.start;
   }
 
   public Timestamp getEndTimestamp() {
-    throw new IllegalStateException("TODO");
+    return this.end;
   }
 
   @Override
--- src/main/java/pl/edu/mimuw/BankClient.java
+++ src/main/java/pl/edu/mimuw/BankClient.java
@@ -1,35 +1,96 @@
 package pl.edu.mimuw;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class BankClient {
 
-  // TODO:
-  // - create proper constructor
-  // - implement methods
-  // - add your own methods (1 is enough)
+  private final String firstName;
+  private final String lastName;
+  private final int age;
+  private final String accountNumber;
+  private final List<BankAction> actions;
+
+  public BankClient(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+
+    var sb = new StringBuilder();
+    var randomGenerator = new Random();
+    for (int i = 0; i < 10; i++) {
+      sb.append(randomGenerator.nextInt(10));
+    }
+    this.accountNumber = sb.toString();
+
+    this.actions = new ArrayList<BankAction>();
+  }
+
+  public BankClient(BankClient client) {
+    this.firstName = client.firstName;
+    this.lastName = client.lastName;
+    this.age = client.age;
+    this.accountNumber = client.accountNumber;
+    this.actions = client.actions;
+  }
 
   public String getName() {
-    throw new IllegalStateException("TODO");
+    return this.firstName + " " + this.lastName;
   }
 
   public String getSurname() {
-    throw new IllegalStateException("TODO");
+    return this.lastName;
   }
 
   public int getAge() {
-    throw new IllegalStateException("TODO");
+    return this.age;
   }
 
   public String getAccountNumber() {
-    throw new IllegalStateException("TODO");
-  }
-
-  public List<BankAction> getActionHistory() {
-    throw new IllegalStateException("TODO");
+    return this.accountNumber;
   }
 
   public void addAction(BankAction action) {
-    throw new IllegalStateException("TODO");
+    this.actions.add(action);
   }
+
+  public List<BankAction> getActionHistory() {
+    final var newActions = new ArrayList<BankAction>();
+
+    for (var action: this.actions) {
+      newActions.add(action.copy());
+    }
+
+    return newActions;
+  }
+
+  public List<BankAction> getActions() {
+    return this.actions;
+  }
+
+  protected double sumActions(Class<?> cls) {
+    double sum = 0;
+
+    for (var deposit: this.actions) {
+      if (deposit.getClass() == cls) {
+        sum += deposit.totalAmount();
+      }
+    }
+
+    return sum;
+  }
+
+  public double depositsSum() {
+    return this.sumActions(DepositBankAction.class);
+  }
+
+  public double loansSum() {
+    return this.sumActions(LoanBankAction.class);
+  }
+
+  public BankClient copy() {
+    return new BankClient(this);
+  }
+
 }
